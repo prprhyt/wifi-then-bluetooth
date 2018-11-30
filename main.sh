40c7a6b8e6aaed2746f6f6327e031708d278c6f7
prprhyt <prprhyt@gmail.com>
2018-11-30 16:43:23 +0900

    delete debug code

--- main.sh
+++ main.sh
@@ -2,5 +2,5 @@
 
 list="ssid1 ssid2"
 connected_ssid=`networksetup -getairportnetwork  en0  | awk '{print $4}'`
-echo $connected_ssid
+#echo $connected_ssid
 [[ $list =~ (^|[[:space:]])$connected_ssid($|[[:space:]]) ]] && /usr/local/bin/blueutil -p 1
