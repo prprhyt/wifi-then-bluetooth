f56f8b33a662ee44c1ad281dbb9dd05ac0fb33f9
prprhyt <prprhyt@gmail.com>
2018-11-30 16:50:31 +0900

    bluetooth turn off when connected ssid that is not registered.

--- main.sh
+++ main.sh
@@ -2,5 +2,4 @@
 
 list="ssid1 ssid2"
 connected_ssid=`networksetup -getairportnetwork  en0  | awk '{print $4}'`
-#echo $connected_ssid
-[[ $list =~ (^|[[:space:]])$connected_ssid($|[[:space:]]) ]] && /usr/local/bin/blueutil -p 1
+[[ $list =~ (^|[[:space:]])$connected_ssid($|[[:space:]]) ]] && /usr/local/bin/blueutil -p 1 || /usr/local/bin/blueutil -p 0
