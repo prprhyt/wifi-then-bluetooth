64d3b893e3dd4743b8e36b5740cee3c4f4b63313
prprhyt <prprhyt@gmail.com>
2018-11-30 16:42:08 +0900

    fix blueutil path

--- main.sh
+++ main.sh
@@ -3,4 +3,4 @@
 list="ssid1 ssid2"
 connected_ssid=`networksetup -getairportnetwork  en0  | awk '{print $4}'`
 echo $connected_ssid
-[[ $list =~ (^|[[:space:]])$connected_ssid($|[[:space:]]) ]] && blueutil -p 1
+[[ $list =~ (^|[[:space:]])$connected_ssid($|[[:space:]]) ]] && /usr/local/bin/blueutil -p 1
